33e6b7e2a80cd5da3512987bff3bb2ca2c0ad3c3
WellSu72 <wellsu@hotmail.com>
2024-05-04 08:52:04 -0300

    Ajuste na estrutura da tomada de decisao pois estava faltando o comando 'Break'

--- src/index.js
+++ src/index.js
@@ -17,13 +17,13 @@ let nickName = getFirstName(jogador);
 
 for(i=0; i < nivelRankeados.length; i++){
   switch(nivelRankeados[i]){
-    case 1: if(saldoVitorias  <= 10){ nivel = "Ferro"} //Se vitórias for menor do que 10 = Ferro
-    case 2: if((saldoVitorias >= 11) && (saldoVitorias <= 20)){ nivel = "Bronze"} //Se vitórias for entre 11 e 20 = Bronze
-    case 3: if((saldoVitorias >= 21) && (saldoVitorias <= 50)){ nivel = "Prata"} //Se vitórias for entre 21 e 50 = Prata
-    case 4: if((saldoVitorias >= 51) && (saldoVitorias <= 80)){ nivel = "Ouro"} //Se vitórias for entre 51 e 80 = Ouro
-    case 5: if((saldoVitorias >= 81) && (saldoVitorias <= 90)){ nivel = "Diamante"} //Se vitórias for entre 81 e 90 = Diamante
-    case 6: if((saldoVitorias >= 91) && (saldoVitorias <= 100)){ nivel = "Lendário"} //Se vitórias for entre 91 e 100= Lendário
-    case 7: if(saldoVitorias  >= 101){ nivel = "Imortal"} //Se vitórias for maior ou igual a 101 = Imortal
+    case 1: if(saldoVitorias  <= 10){ nivel = "Ferro"; break;} //Se vitórias for menor do que 10 = Ferro
+    case 2: if((saldoVitorias >= 11) && (saldoVitorias <= 20)){ nivel = "Bronze"; break;} //Se vitórias for entre 11 e 20 = Bronze
+    case 3: if((saldoVitorias >= 21) && (saldoVitorias <= 50)){ nivel = "Prata"; break;} //Se vitórias for entre 21 e 50 = Prata
+    case 4: if((saldoVitorias >= 51) && (saldoVitorias <= 80)){ nivel = "Ouro"; break;} //Se vitórias for entre 51 e 80 = Ouro
+    case 5: if((saldoVitorias >= 81) && (saldoVitorias <= 90)){ nivel = "Diamante"; break;} //Se vitórias for entre 81 e 90 = Diamante
+    case 6: if((saldoVitorias >= 91) && (saldoVitorias <= 100)){ nivel = "Lendário"; break;} //Se vitórias for entre 91 e 100= Lendário
+    case 7: if(saldoVitorias  >= 101){ nivel = "Imortal"; break;} //Se vitórias for maior ou igual a 101 = Imortal
   }
 }
 
